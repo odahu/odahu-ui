e227376c54a9a70427e33be8bf5a82daa663d5fc
Dmitriy Karbyshev <-z>
2020-06-05 22:52:05 +0300

    [#153] Use pod_name as filter

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -41,7 +41,8 @@ export const LogsDashboard: React.FC<LogsProps> = ({logsURL}) => {
 export function createLogsURL(baseLogsURL: string, parameters: Record<string, string | any>): string {
     let filter_key = Object.keys(parameters)[0];
     let filter_val = Object.values(parameters)[0];
-    let a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:cd0d94c2-7605-430c-9a9c-9cfd0b66d70e,w:48,x:0,y:0),id:f6f6e5e0-95b0-11ea-b67b-07a8a3aceb39,panelIndex:cd0d94c2-7605-430c-9a9c-9cfd0b66d70e,type:search,version:'7.6.2')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filter_val).replace(/KEY/g, filter_key);
+    let a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),type:search,version:'7.6.2')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filter_val).replace(/KEY/g, filter_key).replace(/PANEL_ID/g, panel_id);
+
     return baseLogsURL + '?' + a + '&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))'
 }
 
