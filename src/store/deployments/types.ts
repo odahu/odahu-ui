0e76c6f307197a27b2d060d5efa07410418c79ef
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-09-28 13:36:26 +0300

    Add selectors for deployment state [#11 fix]

--- src/store/deployments/types.ts
+++ src/store/deployments/types.ts
@@ -1,8 +1,17 @@
 import {ModelDeployment} from "../../models/odahuflow/ModelDeployment";
+import {ApplicationState} from "../index";
 
 export interface ModelDeploymentState {
     readonly loading: boolean;
     readonly data: Record<string, ModelDeployment>;
     readonly length: number;
     readonly error?: string;
+}
+
+export const MDSSelector = (state: ApplicationState) => state.deployments
+
+
+export const createMDSelector = (id: string) => (state: ApplicationState): ModelDeployment => {
+    const mdState = MDSSelector(state)
+    return mdState.data[id] ?? {}
 }
