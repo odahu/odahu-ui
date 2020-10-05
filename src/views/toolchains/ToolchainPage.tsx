24264ce6b6d6aae0b83f01f1ad19ff28313dad09
keshamin <keshamin@mail.ru>
2020-10-05 20:52:19 +0300

    Fixed routing for Toolchain and Packaging sections (#64)
    The cause of the bug was a missing parameter baseUrl, that is used for building links for tab buttons like View/YAML/...
    
    Co-authored-by: Innokentii Min <innokentii_min@epam.com>

--- src/views/toolchains/ToolchainPage.tsx
+++ src/views/toolchains/ToolchainPage.tsx
@@ -5,18 +5,21 @@ import {ToolchainView} from "./ToolchainView";
 import {Editor} from "../../components/Editor";
 import {useFetchingEntity} from "../../components/EntitiyFetching";
 import {fetchToolchainRequest} from "../../store/toolchains/actions";
+import {ToolchainURLs} from "./urls";
 
 const tabHeaders = ["View", "YAML"];
 
 export const ToolchainPage: React.FC = () => {
     const {id} = useParams();
     const {entity, loading, notFound} = useFetchingEntity(id as string, fetchToolchainRequest);
+    const baseUrl = `${ToolchainURLs.Page}/${id}`
 
     return (
         <ViewPage
             loading={loading}
             notFound={notFound}
             tabHeaders={tabHeaders}
+            baseUrl={baseUrl}
             tabValues={[
                 <ToolchainView
                     key="view"
