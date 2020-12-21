9ed77f4ae66761e3007b1c51c7d13f0f2a8f288c
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-12-21 12:16:20 +0300

    API to get services outside of reducer

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -12,6 +12,24 @@ import {TrainingService} from "./services/trainings";
 import {TrainingToolchainService} from "./services/toolchain";
 import {UserService} from "./services/user";
 
+let services: Services;
+
+export function getServices(): Services {
+    if (services !== null) {
+        return services
+    }
+    services = {
+        connectionService: new ConnectionService(),
+        configurationService: new ConfigurationService(),
+        userService: new UserService(),
+        deploymentService: new DeploymentService(),
+        packagingService: new PackagingService(),
+        packagerService: new PackagerIntegrationService(),
+        trainingService: new TrainingService(),
+        toolchainService: new TrainingToolchainService(),
+    }
+    return services
+}
 
 export function configureStore(): Store<ApplicationState> {
 
@@ -23,19 +41,12 @@ export function configureStore(): Store<ApplicationState> {
     }
     const composeEnhancers = composeWithDevTools(opts)
 
+    services = getServices()
+
     return createStore(
         createRootReducer,
         composeEnhancers(
-            applyMiddleware(thunk.withExtraArgument<Services>({
-                connectionService: new ConnectionService(),
-                configurationService: new ConfigurationService(),
-                userService: new UserService(),
-                deploymentService: new DeploymentService(),
-                packagingService: new PackagingService(),
-                packagerService: new PackagerIntegrationService(),
-                trainingService: new TrainingService(),
-                toolchainService: new TrainingToolchainService(),
-            }))
+            applyMiddleware(thunk.withExtraArgument<Services>(services))
         )
     );
 }
