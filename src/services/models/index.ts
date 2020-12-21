c44e4570a95f57b301e5f80c3d7def718b0043b1
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-12-21 12:26:05 +0300

    ModelService WIP

---
+++ src/services/models/index.ts
@@ -0,0 +1,13 @@
+
+
+export class ModelsService {
+    protected readonly baseURL: string;
+
+    constructor(baseURL = '') {
+        this.baseURL = baseURL
+    }
+
+    // getInfo(id: string): Promise<> {
+    //
+    // }
+}
