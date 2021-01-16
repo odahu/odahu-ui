2a02f5b0a570d39ac86e42b664a282d841fbe7dc
Dmitriy Karbyshev <-z>
2021-01-16 20:53:35 +0300

    [#45] add logs tab to deployments

--- src/views/deployments/DeploymentViewPage.tsx
+++ src/views/deployments/DeploymentViewPage.tsx
@@ -13,11 +13,16 @@ import {DeploymentView} from "./DeploymentView";
 import {EditableDeploymentPage} from "./DeploymentPages";
 import {useFetchingEntity} from "../../components/EntitiyFetching";
 import {createDashboardURL, GrafanaDashboard} from "../../components/Dashboard";
+import {createLogsURL, LogsDashboard} from "../../components/Dashboard";
+import {useSelector} from "react-redux";
+import {ApplicationState} from "../../store";
 import {DeploymentURLs} from "./urls";
 import {PlayModel} from "../../components/PlayModel";
-
+import {createKibanaEnabledSelector} from "../../store/configuration/types";
+ 
 export const DeploymentViewPage: React.FC = () => {
     const {id} = useParams();
+    const kibanaEnabled = useSelector<ApplicationState, boolean>(createKibanaEnabledSelector())
     const {entity, loading, notFound, setEntity} = useFetchingEntity(id as string, fetchDeploymentRequest);
     const baseUrl = `${DeploymentURLs.Page}/${id}`
     const history = useHistory();
@@ -30,44 +35,80 @@ export const DeploymentViewPage: React.FC = () => {
             history.push(baseUrl)
         }
     );
-
+    const  tabValues = kibanaEnabled ? [
+              <DeploymentView
+                  key="view"
+                  deployment={entity}
+                  status={true}
+              />,
+              <EditableDeploymentPage
+                  key="page"
+                  deployment={entity}
+                  saveButtonClick={saveButtonClick}
+              />,
+              <Editor
+                  key="yaml"
+                  readonly={false}
+                  entity={entity}
+                  fileName={`${id}.deployment.odahuflow.yaml`}
+                  saveButtonClick={saveButtonClick}
+              />,
+              <LogsDashboard
+                 key="logs"
+                 logsURL={createLogsURL(
+                   "/kibana/app/kibana#/dashboard/7390fea0-437c-11eb-8a96-eb5403b48b6c",
+                   {"ModelName": entity.id}
+                 )}
+              />,
+              <GrafanaDashboard
+                  key="grafana"
+                  dashboardURL={createDashboardURL(
+                      "/grafana/d/AdyEtcSZk/model-deployments",
+                      {workload: entity.status?.deployment}
+                  )}
+              />,
+              <PlayModel
+                  key="play"
+                  deployment={entity}
+              /> ]: [
+              <DeploymentView
+                key="view"
+                deployment={entity}
+                status={true}
+              />,
+              <EditableDeploymentPage
+                  key="page"
+                  deployment={entity}
+                  saveButtonClick={saveButtonClick}
+              />,
+              <Editor
+                  key="yaml"
+                  readonly={false}
+                  entity={entity}
+                  fileName={`${id}.deployment.odahuflow.yaml`}
+                  saveButtonClick={saveButtonClick}
+              />,
+              <GrafanaDashboard
+                  key="grafana"
+                  dashboardURL={createDashboardURL(
+                      "/grafana/d/AdyEtcSZk/model-deployments",
+                      {workload: entity.status?.deployment}
+                  )}
+              />,
+              <PlayModel
+                  key="play"
+                  deployment={entity}
+              />
+    ]
+    const tabHeaders = kibanaEnabled ? ["View", "Edit", "YAML", "logs", "Dashboard", "Play"] : ["View", "Edit", "YAML", "Dashboard", "Play"]
 
     return (
         <ViewPage
             loading={loading}
             notFound={notFound}
-            tabHeaders={["View", "Edit", "YAML", "Dashboard", "Play"]}
+            tabHeaders={tabHeaders}
             baseUrl={baseUrl}
-            tabValues={[
-                <DeploymentView
-                    key="view"
-                    deployment={entity}
-                    status={true}
-                />,
-                <EditableDeploymentPage
-                    key="page"
-                    deployment={entity}
-                    saveButtonClick={saveButtonClick}
-                />,
-                <Editor
-                    key="yaml"
-                    readonly={false}
-                    entity={entity}
-                    fileName={`${id}.deployment.odahuflow.yaml`}
-                    saveButtonClick={saveButtonClick}
-                />,
-                <GrafanaDashboard
-                    key="grafana"
-                    dashboardURL={createDashboardURL(
-                        "/grafana/d/AdyEtcSZk/model-deployments",
-                        {workload: entity.status?.deployment}
-                    )}
-                />,
-                <PlayModel
-                    key="play"
-                    deployment={entity}
-                />
-            ]}
+            tabValues={tabValues}
         />
     )
 };
