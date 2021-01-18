deed2f9fe19511b3bba53ef02091bb71b1504e05
Dmitriy Karbyshev <-z>
2021-01-18 03:11:17 +0300

    [#45] fix deployment kibana embed URL

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -41,7 +41,7 @@ export const LogsDashboard: React.FC<LogsProps> = ({logsURL}) => {
 export function createLogsURL(baseLogsURL: string, parameters: Record<string, string | any>): string {
     const filterKey = Object.keys(parameters)[0];
     const filterVal = Object.values(parameters)[0];
-    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,w:48,x:0,y:0),id:e0d473a0-a6c1-11ea-bc41-3318d1349021,panelIndex:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,type:search,version:'7.6.2')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
+    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'a637bcb2-4c05-4ad8-ad59-89cb37c9c66c',w:48,x:0,y:0),id:'e0d473a0-a6c1-11ea-bc41-3318d1349021',panelIndex:'a637bcb2-4c05-4ad8-ad59-89cb37c9c66c',type:search,version:'7.6.2')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
 
 
     return baseLogsURL + '?' + a + '&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))'
