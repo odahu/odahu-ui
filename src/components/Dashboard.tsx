01a16d88ede6ffb9bad6cf7d388f57fa5c0da095
Dmitriy Karbyshev <-z>
2021-02-09 19:45:25 +0300

    [#39] Refresh logs in embedded tabs every 5 seconds

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -41,7 +41,7 @@ export const LogsDashboard: React.FC<LogsProps> = ({logsURL}) => {
 export function createLogsURL(baseLogsURL: string, parameters: Record<string, string | any>): string {
     const filterKey = Object.keys(parameters)[0];
     const filterVal = Object.values(parameters)[0];
-    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,w:48,x:0,y:0),id:e0d473a0-a6c1-11ea-bc41-3318d1349021,panelIndex:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,type:search,version:'7.6.2')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
+    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:logstash,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:25,i:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,w:48,x:0,y:0),id:e0d473a0-a6c1-11ea-bc41-3318d1349021,panelIndex:a637bcb2-4c05-4ad8-ad59-89cb37c9c66c,type:search,version:'7.6.2')),interval:auto,query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
 
     return baseLogsURL + '?' + a + '&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:5000),time:(from:now-1y,to:now))'
 }
@@ -49,7 +49,7 @@ export function createLogsURL(baseLogsURL: string, parameters: Record<string, st
 export function createDeploymentLogsURL(baseLogsURL: string, parameters: Record<string, string | any>): string {
     const filterKey = Object.keys(parameters)[0];
     const filterVal = Object.values(parameters)[0];
-    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:deployment,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'366684df-f1ad-4d30-9916-2e650891b3f1',w:48,x:0,y:0),id:'6fa14de0-410b-11eb-ade2-fd5594ed8d63',panelIndex:'366684df-f1ad-4d30-9916-2e650891b3f1',type:search,version:'7.9.0')),query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
+    const a = "_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:deployment,key:KEY,negate:!f,params:(query:VALUE),type:phrase),query:(match_phrase:(KEY:VALUE)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:25,i:'366684df-f1ad-4d30-9916-2e650891b3f1',w:48,x:0,y:0),id:'6fa14de0-410b-11eb-ade2-fd5594ed8d63',panelIndex:'366684df-f1ad-4d30-9916-2e650891b3f1',type:search,version:'7.9.0')),interval:auto,query:(language:kuery,query:''),timeRestore:!f,title:'',viewMode:view)&query='KEY:VALUE'".replace(/VALUE/g, filterVal).replace(/KEY/g, filterKey);
 
     return baseLogsURL + '?' + a + '&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:5000),time:(from:now-1y,to:now))'
 }
